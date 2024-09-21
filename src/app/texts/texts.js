265b88a97c146265dec9f27f9c4f7929f3e41ccd
digitalpruu <digitalpruu@gmail.com>
2024-09-21 03:24:10 -0500

    Primer commit con el nuevo nombre del repositorio

---
+++ src/app/texts/texts.js
@@ -0,0 +1,37 @@
+const title = "OXXFOR.A";
+const email = ""; // Información no proporcionada
+const mensajeWhatsApp = encodeURIComponent(
+    "¡Hola! Estoy muy interesado en tus productos de " + title + " y me gustaría recibir más información sobre ellos. Espero poder hablar pronto contigo para aclarar algunas dudas. ¡Gracias!"
+);
+const numeroWhatsApp = ""; // Información no proporcionada
+
+const textos = {
+    index: {
+        title: title,
+    },
+    homeContent: {
+        title: title,
+    },
+    about: {
+        description1: "OXXFOR.A se especializa en la venta de blusas con un enfoque en la moda y el estilo. Nuestro objetivo es ofrecer prendas elegantes y versátiles que se adapten a diferentes ocasiones y gustos. Cada blusa está diseñada para proporcionar confort y un toque de distinción.",
+        description2: "En OXXFOR.A, nos enorgullece brindar productos de alta calidad que complementan tu guardarropa. Visita nuestra página web para explorar nuestra colección y encontrar la blusa perfecta que se ajuste a tu estilo personal."
+    },
+    contact: {
+        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
+        email: email
+    },
+    footer: {
+        slogan: {
+            p1: "Elegancia y estilo en cada blusa",
+            p2: ""
+        },
+        address: "", // Información no proporcionada
+        phone: numeroWhatsApp,
+        email: email,
+        facebook: "https://www.facebook.com/",
+        instagram: "https://www.instagram.com/",
+        whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`
+    },
+};
+
+export default textos;
